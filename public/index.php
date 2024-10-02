69a0a0c667c90f774ded57c4e0e6ed7b4822a77e
sasha <sander_@inbox.ru>
2024-10-02 21:56:08 +0300

    added template

--- public/index.php
+++ public/index.php
@@ -5,14 +5,21 @@ namespace Hexlet\Project;
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+
+$container->set('renderer', function() {
+    return new \Slim\Views\PhpRenderer('../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Hexlet project 3');
-    return $response;
+    $params = [];
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
 });
 
 $app->run();
