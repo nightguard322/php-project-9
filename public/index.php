cb2e56cea8d624350a2db20ecbb04e946d0b3a89
sasha <sander_@inbox.ru>
2024-10-02 14:59:28 +0300

    added first route

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Hexlet\Project;
+
+require __DIR__ . '../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function($request, $response) {
+    $response->getBody()->write('Hexlet project 3');
+    return $response;
+});
+
+$app->run();
